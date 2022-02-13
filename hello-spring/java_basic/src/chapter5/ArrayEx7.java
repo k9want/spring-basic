22cd7bbcd5c2ac90f72c6de4be2d75dd21559d59
qma <zndwlrnjr26@daum.net>
2022-02-14 01:01:42 +0900

    배열의 요소 섞기 예제

---
+++ hello-spring/java_basic/src/chapter5/ArrayEx7.java
@@ -0,0 +1,26 @@
+package chapter5;
+
+import java.util.Arrays;
+
+public class ArrayEx7 {
+    public static void main(String[] args) {
+        int[] numArr = new int[10];
+
+        for (int i = 0; i < numArr.length; i++) {
+            numArr[i] = i;
+            System.out.print(numArr[i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < numArr.length; i++) {
+            int n = (int)(Math.random()*10);
+            int tmp = numArr[0];
+            numArr[0] = numArr[n];
+            numArr[n] = tmp;
+        }
+//        System.out.println(Arrays.toString(numArr));
+        for (int i = 0; i < numArr.length; i++) {
+            System.out.print(numArr[i]);
+        }
+    }
+}
