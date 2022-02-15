63e045d7cd467368c44d021897e43bd7de58791b
qma <zndwlrnjr26@daum.net>
2022-02-16 00:09:26 +0900

    재귀호출 예제 - 팩토리얼(수정)

--- hello-spring/java_basic/src/chapter6/FactorialTest.java
+++ hello-spring/java_basic/src/chapter6/FactorialTest.java
@@ -9,11 +9,11 @@ public class FactorialTest {
 
     static int factorial(int n) {
         int result = 0;
-        if (n == 1) {
+        if (n == 1)
             return 1;
-        } else {
-            result = n * factorial(n - 1);
-        }
+
+        result = n * factorial(n - 1);
+
         return result;
     }
 }
