3880989654502368f0afb232026d06d012d00ea1
qma <zndwlrnjr26@daum.net>
2022-02-17 01:11:26 +0900

    초기화 블럭 예제

---
+++ hello-spring/java_basic/src/chapter6/ProductTest.java
@@ -0,0 +1,25 @@
+package chapter6;
+
+class Product {
+    static int count = 0;
+    int serialNo;
+
+    {
+        count++;
+        serialNo = count;
+    }
+
+}
+
+class ProductTest {
+    public static void main(String[] args) {
+        Product p1 = new Product();
+        Product p2 = new Product();
+        Product p3 = new Product();
+
+        System.out.println(p1.serialNo);
+        System.out.println(p2.serialNo);
+        System.out.println(p3.serialNo);
+    }
+}
+
--- hello-spring/java_basic/src/chapter6/StaticBlockTest.java
+++ hello-spring/java_basic/src/chapter6/StaticBlockTest.java
@@ -10,7 +10,6 @@ public class StaticBlockTest {
         }
     }
 
-
     public static void main(String[] args) {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
