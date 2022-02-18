9150995d4e6efc0a8b5c1583e5d3ee03afd90045
qma <zndwlrnjr26@daum.net>
2022-02-19 02:25:37 +0900

    참조변수 Instanceof 타입,  true면 해당 타입으로 형변환이 가능

---
+++ hello-spring/java_basic/src/chapter7/InstanceofTest.java
@@ -0,0 +1,20 @@
+package chapter7;
+
+public class InstanceofTest {
+    public static void main(String[] args) {
+         FireEngine fe = new FireEngine();
+
+        if (fe instanceof FireEngine) {
+            System.out.println("FireEngine instance");
+        }
+
+        if (fe instanceof Car) {
+            System.out.println("Car instance");
+        }
+
+        if (fe instanceof Object) {
+            System.out.println("Object instance");
+        }
+        System.out.println(fe.getClass().getName());
+    }
+}
