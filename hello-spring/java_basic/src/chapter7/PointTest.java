50eb93d1e69c91ccf77fb7548604d81ea638df01
qma <zndwlrnjr26@daum.net>
2022-02-18 20:34:40 +0900

    super(), this()

---
+++ hello-spring/java_basic/src/chapter7/PointTest.java
@@ -0,0 +1,40 @@
+package chapter7;
+
+class Point7 {
+    int x, y;
+
+
+    public Point7(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    String getLocation() {
+        return " x : "+ x + " y : " + y;
+    }
+}
+
+class Point3D extends Point7 {
+    int z;
+
+    Point3D() {
+        this(1,1,1);
+    }
+
+    public Point3D(int x, int y, int z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    String getLocation() {
+        return super.getLocation() + " z : " + z;
+    }
+}
+
+
+public class PointTest {
+    public static void main(String[] args) {
+        Point3D p3 = new Point3D();
+        System.out.println(p3.getLocation());
+    }
+}
