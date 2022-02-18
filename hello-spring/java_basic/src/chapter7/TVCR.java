8df35968847033b5be9d915fe191fbd2a7ad45f0
qma <zndwlrnjr26@daum.net>
2022-02-18 19:43:57 +0900

    단일상속

---
+++ hello-spring/java_basic/src/chapter7/TVCR.java
@@ -0,0 +1,53 @@
+package chapter7;
+
+class TV {
+    boolean power;
+    int channel;
+
+    void power() {
+        power = !power;
+    }
+    void channelUp() {
+        ++channel;
+    }
+    void channelDown() {
+        --channel;
+    }
+}
+
+class VCR {
+    boolean power;
+    int counter = 0;
+    void power() {
+        power = !power;
+    }
+    void play() {
+        System.out.println("play");
+    }
+    void stop() {
+        System.out.println("stop");
+    }
+    void rew() {
+        System.out.println("rew");
+    }
+    void ff() {
+        System.out.println("ff");
+    }
+}
+
+public class TVCR {
+    VCR vcr = new VCR();
+
+    void play() {
+        vcr.play();
+    }
+    void stop() {
+        vcr.stop();
+    }
+    void rew() {
+        vcr.rew();
+    }
+    void ff() {
+        vcr.ff();
+    }
+}
