f6b20d0779c9d8049e453b992c7fb5f1592d946b
qma <zndwlrnjr26@daum.net>
2022-02-19 01:40:44 +0900

    참조변수의 형변환은 인스턴스에 영향을 미치지 않는다. 참조하고 있는 인스턴스에서 사용할 수 있는 멤버의 범위를 조절만 한다.

---
+++ hello-spring/java_basic/src/chapter7/CastingTest1.java
@@ -0,0 +1,34 @@
+package chapter7;
+
+class Car {
+    String color;
+    int door;
+
+    void drive() {
+        System.out.println("drive, go~");
+    }
+
+    void stop() {
+        System.out.println("stop!");
+    }
+}
+
+class FireEngine extends Car {
+    void water() {
+        System.out.println("water~~!!");
+    }
+}
+
+public class CastingTest1 {
+    public static void main(String[] args) {
+        Car car = null;
+        FireEngine fe = new FireEngine();
+        FireEngine fe2 = null;
+
+        fe.water();
+        car = fe;
+//        car.water() //car 잠조변수로는 불가능
+        fe2 = (FireEngine) car;
+        fe2.water();
+    }
+}
