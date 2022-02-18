d80723c3468541e357b12c84596638d189b211fa
qma <zndwlrnjr26@daum.net>
2022-02-18 23:28:04 +0900

    FinalCardTest 코드 추가(수정)

--- hello-spring/java_basic/src/chapter7/FinalCardTest.java
+++ hello-spring/java_basic/src/chapter7/FinalCardTest.java
@@ -24,9 +24,10 @@ public class FinalCardTest {
         Card7 c = new Card7();
         System.out.println(c.toString());
 
-        Card7 c2 = new Card7();
+        Card7 c2 = new Card7(4,"HEART");
         //에러나는 이유 final 변수인데 즉 상수인데, 이미 생성자를 통해 초기화를 했기때문에 추가적으로 값을 변경할 수 없다.
         //c2.NUMBER = 5;
+        System.out.println(c2.toString());
 
     }
 
