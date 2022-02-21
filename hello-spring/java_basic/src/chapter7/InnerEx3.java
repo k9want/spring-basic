d27bc2835246131446c4a3afe4e09728790c1b36
qma <zndwlrnjr26@daum.net>
2022-02-22 02:03:24 +0900

    내부클래스에서 외부클래스의 변수들에 접근성을 보여주는 예제

---
+++ hello-spring/java_basic/src/chapter7/InnerEx3.java
@@ -0,0 +1,30 @@
+package chapter7;
+
+public class InnerEx3 {
+    private int outerIv = 0;
+    static int outerCv = 0;
+
+    class InstanceInner3 {
+        int iiv = outerIv;
+        int iiv2 = outerCv;
+    }
+
+    static class StaticInner {
+        static int scv = outerCv;
+    }
+
+    void myMethod() {
+        int lv = 0;
+        final int LV = 0;
+
+        class LocalInner {
+            int liv = outerIv;
+            int liv2 = outerCv;
+            int liv3 = lv;
+            int liv4 = LV;
+        }
+
+    }
+
+
+}
