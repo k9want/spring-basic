12e5e4a52df03c0703193c148c91b5f05f12ef4b
qma <zndwlrnjr26@daum.net>
2022-02-22 00:21:28 +0900

    인터페이스를 이용해서 직접적인관계인 두 클래스를 간접적인 관계로 바꿈

--- hello-spring/java_basic/src/chapter7/InterfaceTest.java
+++ hello-spring/java_basic/src/chapter7/InterfaceTest.java
@@ -1,16 +1,22 @@
 package chapter7;
 
+//직접적인 관계인 두 클래스를 인터페이스를 이용해서 간접적인 관계로 바꾸기 (인터페이스를 이용해서 선언과 구현을 분리한다.)
+interface I {
+    public abstract void methodB();
+}
+
 class A {
-    public static void methodA(B b) {
-        b.methodB();
+    public static void methodA(I i) {
+        i.methodB();
     }
 }
 
-class B {
+class B implements I {
     public void methodB() {
         System.out.println("methodB()");
     }
 }
+
 
 public class InterfaceTest {
     public static void main(String[] args) {
