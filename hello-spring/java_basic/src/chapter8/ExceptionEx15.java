301d759e180fa72515342fd0f14e049b29661d18
qma <zndwlrnjr26@daum.net>
2022-02-28 02:21:04 +0900

    메서드에 호출 시 넘겨받아야할(fileName)을 다시 받아야하는 경우 (메서드 내에서 자체적으로 해결이 안되는 경우) 예외를 메서드에 선언해서, 호출한 메서드에서 처리해야한다.

--- hello-spring/java_basic/src/chapter8/ExceptionEx15.java
+++ hello-spring/java_basic/src/chapter8/ExceptionEx15.java
@@ -5,30 +5,20 @@ import java.io.IOException;
 
 public class ExceptionEx15 {
     public static void main(String[] args) {
-        File f = createFile("");
-        System.out.println(f.getName() + "파일이 생성되었습니다.");
-    }
-
-    static File createFile(String fileName) {
         try {
-            if (fileName == null || fileName.equals("")) {
-                throw new Exception("파일이름이 유효하지 않습니다");
-            }
+            File f = createFile("");
+            System.out.println(f.getName() + "파일이 생성되었습니다.");
         } catch (Exception e) {
-//            e.printStackTrace();
-            fileName = "제목없음.txt";
-        } finally {
-            File f = new File(fileName);
-            createNewFile(f);
-            return f;
+            System.out.println(e.getMessage() + "다시 입력해주세요");
         }
     }
 
-    static void createNewFile(File f) {
-        try {
-            f.createNewFile();
-        } catch (IOException e) {
-//            e.printStackTrace();
+    static File createFile(String fileName) throws Exception {
+        if (fileName == null || fileName.equals("")) {
+            throw new Exception("파일이름이 유효하지 않습니다");
         }
+        File f = new File(fileName);
+        f.createNewFile();
+        return f;
     }
 }
