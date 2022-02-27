7fc2eb3bd18a140b4ac72e366a0c1c9befe604bc
qma <zndwlrnjr26@daum.net>
2022-02-28 01:58:48 +0900

    하지만 어느 한곳에서는 try-catch문으로 예외처리를 해줘야한다.

--- hello-spring/java_basic/src/chapter8/ExceptionEx12.java
+++ hello-spring/java_basic/src/chapter8/ExceptionEx12.java
@@ -3,9 +3,14 @@ package chapter8;
 public class ExceptionEx12 {
     public static void main(String[] args) throws Exception{
         method1();
+        System.out.println("실행 종료");
     }
     static void method1() throws Exception {
-        method2();
+        try {
+            method2();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private static void method2() throws Exception {
