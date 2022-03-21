1d7f1b26d1daef5e611d3d7dca703b808dab7324
qma <zndwlrnjr26@daum.net>
2022-03-21 22:33:41 +0900

    일반적으로 배열을 복사할 때는 같은 길이의 새로운 배열을 생성한 다움에 System.arraycopy()를 이용해서 내용을 복사한다.

--- hello-spring/java_basic/src/chapter9/CloneEx2.java
+++ hello-spring/java_basic/src/chapter9/CloneEx2.java
@@ -9,5 +9,10 @@ public class CloneEx2 {
         arrClone[0] = 6;
         System.out.println(Arrays.toString(arr));
         System.out.println(Arrays.toString(arrClone));
+
+        int[] arrClone2 = new int[arr.length]; //배열생성
+        System.arraycopy(arr,0,arrClone2,0,arr.length); //내용을 복사
+        System.out.println(Arrays.toString(arrClone2));
+
     }
 }
